c35bff8ed2a24e45f7fcac7fb2e5b2842151109f
Dilay Ercelik <63108603+dilayercelik@users.noreply.github.com>
2020-05-09 21:37:13 +0200

    Add files via upload

---
+++ Easy/Strings/string_validators.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    s = input()
+
+print(any(character.isalnum() for character in s))
+print(any(character.isalpha() for character in s))
+print(any(character.isdigit() for character in s))
+print(any(character.islower() for character in s))
+print(any(character.isupper() for character in s))
